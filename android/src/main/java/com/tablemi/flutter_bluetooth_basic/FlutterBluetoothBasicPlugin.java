d05a9c8e323b41f4a19642b6f88aacce7fb1878b
Louis <louis@macbookpro.home>
2025-04-29 16:31:31 +0200

    Remove deprecated plugin registrarWith function

--- android/src/main/java/com/tablemi/flutter_bluetooth_basic/FlutterBluetoothBasicPlugin.java
+++ android/src/main/java/com/tablemi/flutter_bluetooth_basic/FlutterBluetoothBasicPlugin.java
@@ -37,7 +37,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.plugin.common.PluginRegistry.RequestPermissionsResultListener;
 
 /**
@@ -56,14 +55,6 @@ public class FlutterBluetoothBasicPlugin implements FlutterPlugin, MethodCallHan
   private ActivityPluginBinding activityPluginBinding;
 
   private Result pendingResult;
-
-  // plugin should still contain the static registerWith() method to remain compatible with apps
-  // that don’t use the v2 Android embedding.
-  public static void registerWith(Registrar registrar) {
-    FlutterBluetoothBasicPlugin instance = new FlutterBluetoothBasicPlugin();
-    instance.createChannel(registrar.messenger());
-    registrar.addRequestPermissionsResultListener(instance);
-  }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
